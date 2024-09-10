ff6482b4d0121d2eda5b66d229a0898ef69f9604
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2024-09-10 14:41:50 +0530

    responsive of logo and heading

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,7 +7,8 @@ import {
   NavbarItem,
 } from "@nextui-org/react";
 import { ApprikartLogo , XircularLogo} from "./Icons";
-// import { ThemeSwitch } from "./ThemeSwitch";
+import "./navbar.css";
+
 
 export default function NavBar() {
   return (
@@ -16,55 +17,13 @@ export default function NavBar() {
         {/* <Link isExternal aria-label="HeyGen" href="https://app.heygen.com/"> */}
           <ApprikartLogo />
         {/* </Link> */}
-        <div className="bg-gradient-to-br from-sky-300 to-indigo-500 bg-clip-text" style={{marginLeft:"18rem"}}>
+        <div className="bg-gradient-to-br from-sky-300 to-indigo-500 bg-clip-text"id="headingtext" style={{marginLeft:"18rem"}}>
           <p className="text-xl font-semibold text-transparent">
           Interactive Avatar Demo
           </p>
         </div>
         <XircularLogo/>
       </NavbarBrand>
-      <NavbarContent justify="center">
-        <NavbarItem className="flex flex-row items-center gap-4">
-          {/* <Link
-            isExternal
-            color="foreground"
-            href="https://app.heygen.com/interactive-avatar"
-          >
-            Avatars
-          </Link>
-          <Link
-            isExternal
-            color="foreground"
-            href="https://docs.heygen.com/reference/list-voices-v2"
-          >
-            Voices
-          </Link>
-          <Link
-            isExternal
-            color="foreground"
-            href="https://docs.heygen.com/reference/new-session-copy"
-          >
-            API Docs
-          </Link>
-          <Link
-            isExternal
-            color="foreground"
-            href="https://help.heygen.com/en/articles/9182113-interactive-avatar-101-your-ultimate-guide"
-          >
-            Guide
-          </Link>
-          <Link
-            isExternal
-            aria-label="Github"
-            href="https://github.com/HeyGen-Official/StreamingAvatarSDK"
-            className="flex flex-row justify-center gap-1 text-foreground"
-          >
-            <GithubIcon className="text-default-500" />
-            SDK
-          </Link> */}
-          {/* <ThemeSwitch /> */}
-        </NavbarItem>
-      </NavbarContent>
     </Navbar>
   );
 }
